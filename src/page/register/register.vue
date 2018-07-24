5e79b7b886d48692de91fd096a2d26df2d9c69a1
Ry-yuan <1094324110@qq.com>
2018-07-24 12:01:50 +0800

    [add] register

---
+++ src/page/register/register.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- <mu-flex class="register-wrapper flex-wrapper" justify-content="center">
+        <mu-flex class="register-box flex-demo" justify-content="center" >center</mu-flex>
+    </mu-flex> -->
+    <div class="reigster-box">ss</div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+        // this.$el.style.height = documnet.documentElement.clientHeight
+    }
+}
+</script>
+
+<style>
+.register-wrapper{
+    position: relative;
+    height: 300px;
+    /* transform: translateY(50%); */
+}
+
+.register-box{
+     position: relative;
+        top:50%;
+        transform: translateY(-50%);
+        border:1px solid red;
+        height: 200px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Chat from '@/components/chat'
-
+import Register from '../page/register/register'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Chat',
       component: Chat
+    },
+    {
+      path:'/register',
+      name:'Register',
+      component:Register
     }
   ]
 })
